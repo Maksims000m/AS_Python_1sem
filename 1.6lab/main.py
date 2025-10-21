9a307fa554101578bf20ccd7da2baecc1d1cbd8e
Maksims000m <maksim.smolkin@list.ru>
2025-10-21 11:39:32 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
-    pass
+    def i (s):
+    if len(s) % 4 == 0:
+        return s[::-1]
+    else:
+        return s
+    v1="abcd"
+    v2="kolos"
+    r1=i(v1)
+    r2=i(v2)
+    print(r1)
+    print(r2)
