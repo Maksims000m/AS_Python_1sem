8122d88727ec743eb9d913c35e02af3639667184
Maksims000m <maksim.smolkin@list.ru>
2025-10-21 10:51:59 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     for in range (N):
         print(K)
     print("Задание №2")
-    K=int(input("Введите K:"))
-    N=int(input("Введите N:"))
-    for in range (N):
-        print(K)
+
+    
+    A=int(input("Введите A:"))
+    B=int(input("Введите B:"))
+    numbers = list(range(A, B + 1))
+    print(*numbers)
+    print("Количество чисел: ", len(numbers))
