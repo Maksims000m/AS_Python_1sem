2ceac510e3231dac59fde5a03fafc708cda8a79b
Maksims000m <maksim.smolkin@list.ru>
2025-11-06 08:41:18 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,11 +1,8 @@
 def a(lst):
     b = any(num % 2 != 0 for num in lst)
-   
     if b:
         return [0 if num % 2 != 0 else num for num in lst]
-   
     return lst
-    
 c = [1, 2, 3, 4, 5]
 d = a(c)
 print(d)
