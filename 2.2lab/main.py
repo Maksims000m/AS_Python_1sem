c47cae4db9098c63bd76dbccc668bedf7cb0d7aa
Maksims000m <maksim.smolkin@list.ru>
2025-11-18 11:15:07 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    pass
+def quarter(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+    else:
+        raise ValueError("Координаты должны быть ненулевыми.")
+try:
+    x = float(input("Введите координату x: "))
+    y = float(input("Введите координату y: "))
+    result = quarter(x, y)
+    print(f"Точка ({x}, {y}) находится в {result}-й четверти.")
+except ValueError as e:
+    print(e)
