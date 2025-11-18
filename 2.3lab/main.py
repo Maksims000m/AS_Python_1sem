7e83e19c627cb79415619c9b947aafd51e29dc9a
Maksims000m <maksim.smolkin@list.ru>
2025-11-18 10:36:24 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,19 @@
+def SwapCol(A, K1, K2):
+    if K1 < 0 or K2 < 0 or K1 >= len(A[0]) or K2 >= len(A[0]):
+        raise IndexError("Индексы столбцов выходят за пределы матрицы")
+    for row in A:
+        row[K1], row[K2] = row[K2], row[K1]
 if __name__ == "__main__":
-    pass
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print("Первоначальная матрица:")
+    for row in matrix:
+        print(row)
+
+    SwapCol(matrix, 0, 2)
+    print("Матрица после обмена столбцов:")
+    for row in matrix:
+        print(row)
