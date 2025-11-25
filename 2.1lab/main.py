d067cbaa5680c8067a19fb1c27a7483764e03557
Maksims000m <maksim.smolkin@list.ru>
2025-11-25 16:11:51 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    pass
+def a(b):
+    return sum(b.values())
+
+c = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+print("Сумма значений:", a(c))  
+
+def w(v):
+    return {key: {} for key in v}
+
+d = ['a', 'b', 'c']
+print("Словарь из списка:", w(d)) 
+
+def r(b):
+    return len(set(b.values())) == 1
+
+y = {'a': 1, 'b': 1, 'c': 1}
+print("Все значения одинаковые:", r(y))  
+
+ny = {'a': 1, 'b': 2, 'c': 1}
+print("Все значения одинаковые:", r(ny))  
