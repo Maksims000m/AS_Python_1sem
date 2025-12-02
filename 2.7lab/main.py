7a5a19e48b9676afb205781b9d17d6694bee5493
Maksims000m <maksim.smolkin@list.ru>
2025-12-02 11:21:45 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,16 @@
+def count_paragraphs(filename):
+  with open(filename, 'r', encoding='utf-8') as file:
+    content = file.read()
+
+  paragraphs = [p for p in content.split('\n\n') if p.strip()]
+   
+  return len(paragraphs)
+
+def main():
+  filename = 'text.txt'
+  num_paragraphs = count_paragraphs(filename)
+   
+  print(f"Количество абзацев в тексте: {num_paragraphs}")
+
 if __name__ == "__main__":
-    pass
+  main()
